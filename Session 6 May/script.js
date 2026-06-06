5a9342e34c20f7feb95e60356e11a3cb49215b63
justforit4102 <deeppakhare19@gmail.com>
2026-06-06 22:47:57 +0530

    practice session on JS

--- Session 6 May/script.js
+++ Session 6 May/script.js
@@ -17,25 +17,75 @@
 // console.log(average);
 
 // Most Frequent Number
-let numbers = [1, 2, 3, 2,2,2,2,2,2, 4, 2, 5, 1, 1, 1];
+// let numbers = [1, 2, 3, 2, 4, 2, 5, 1, 1, 1];
 
-let count = {};
-for (let i = 0; i <= numbers.length; i++) {
-  let num = numbers[i];
-  if (count[num]) {
-    count[num] = count[num] + 1;
-  } else {
-    count[num] = 1;
-  }
-}
-// console.log(count);
-let frequency = 0;
-let mostFrequent;
-for (let key in count) {
-  if (count[key] > frequency) {
-    frequency = count[key];
-    mostFrequent = key;
-  }
-}
+// let count = {};
+// for (let i = 0; i <= numbers.length; i++) {
+//   let num = numbers[i];
+//   if (count[num]) {
+//     count[num] = count[num] + 1;
+//   } else {
+//     count[num] = 1;
+//   }
+// }
+// // console.log(count);
+// let frequency = 0;
+// let mostFrequent;
+// for (let key in count) {
+//   if (count[key] > frequency) {
+//     frequency = count[key];
+//     mostFrequent = key;
+//   }
+// }
 
-console.log(mostFrequent);
+// console.log(mostFrequent);
+
+//  Updating values
+
+
+// let user = {
+//     name: "Ritik",
+//     age: 20
+// };
+// user.age = 21;
+// console.log(user);
+
+// let user = {
+//     name: 'Ritik',
+//     age: 20,
+//     city: 'Bhopal'
+// };
+// for (const [key, value] of Object.entries(user)) {
+//     console.log(key,value);
+// }
+
+// Higest Paid Employee
+
+// Calculate discount 
+
+// Dynamic sum function 
+
+// function sum(...num){
+//     return num.reduce((acc,val) => {
+//         return acc+val;
+//     },0)
+// }
+// console.log(sum(1,2,3,4,5))
+
+
+// Find Adult Users 
+
+let users = [
+    {name: 'Ritik', age:20},
+    {name: 'Aman', age:16},
+    {name: 'Priya', age:25}
+]
+
+function getAdults(users){
+    return users.filter((users) => {
+        if(users.age>=18) return user
+    })
+}
+console.log(getAdults(users))
+
+
